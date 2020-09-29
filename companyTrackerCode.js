b79ea977efee1996ca124fcab4132f8941687a8a
Andres A <acosta.andres.r@gmail.com>
2020-09-29 14:42:23 -0500

    Displayed View All Employees in table using console.table require.

--- companyTrackerCode.js
+++ companyTrackerCode.js
@@ -129,3 +129,29 @@ function startPrompt() {
     });
 }
 
+function viewAllEmployees() {
+  let query =
+    `SELECT 
+      e.id, 
+      e.first_name, 
+      e.last_name, 
+      r.title, 
+      d.name AS department,
+      r.salary, 
+      CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN employee m ON e.manager_id = m.id
+    JOIN role r ON e.role_id = r.id
+    JOIN department d ON r.department_id = d.id`;
+
+  connection.query(query, function (err, res) {
+    if (err) console.error(err);
+
+    console.log('');
+    console.log('');
+    console.table(res);
+    console.log('');
+
+    startPrompt();
+  });
+}
