f6372d1b714c89c5dc08d1404d11399fe5e2ea67
Andres A <acosta.andres.r@gmail.com>
2020-09-29 13:01:42 -0500

    Created database connection and started list of choices by prompting in the CLI.

---
+++ companyTrackerCode.js
@@ -0,0 +1,110 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// Create a connection to employees database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'employee-tracker',
+  password: '123456',
+  database: 'employeesDB'
+});
+
+// Establishing connection with database
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  // Confimrmation that connection was established correctly
+  console.log('connected as id ' + connection.threadId);
+
+  // Start CLI application
+  runCLIapp();
+});
+
+
+function runCLIapp() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add New Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles",
+        "Add new Role",
+        "Remove Role",
+        "Add New Department",
+        "Remove Department",
+        "View Budget of a Department",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          // console.log("view all employees");
+          // viewAllEmployees();
+          break;
+
+        case "View All Employees by Department":
+
+          break;
+
+        case "View All Employees by Manager":
+
+          break;
+
+        case "Add New Employee":
+
+          break;
+
+        case "Remove Employee":
+
+          break;
+
+        case "Update Employee Role":
+
+          break;
+
+        case "Update Employee Manager":
+
+          break;
+
+        case "View All Roles":
+
+          break;
+
+        case "Add New Role":
+
+          break;
+
+        case "Remove Role":
+
+          break;
+
+        case "Add New Department":
+
+          break;
+
+        case "Remove Department":
+
+          break;
+
+        case "View Budget of a Department":
+
+          break;
+
+        case "Exit":
+
+          break;
+      }
+    });
+}
